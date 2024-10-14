4354e896c6a24d6ae5de9d4e66037873b221ecd5
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2024-10-14 16:00:13 -0700

    Added functions for getting the path and the file name

--- dynamicWebServer.py
+++ dynamicWebServer.py
@@ -8,8 +8,8 @@ Description: Create a basic web server.
 
 """
 SCRATCH
-- [ ] Parse that request header to get the file name.
-- [ ] Strip the path off for security reasons.
+- [x] Parse that request header to get the file name.
+- [x] Strip the path off for security reasons.
 - [ ] Read the data from the named file.
 - [ ] Determine the type of data in the file, HTML or text.
 - [ ] Build an HTTP response packet with the file data in the payload.
@@ -18,6 +18,7 @@ SCRATCH
 """
 import socket
 import sys
+import os
 def command_line_argument_exists():
     return len(sys.argv) == 2
 
@@ -39,7 +40,7 @@ def get_request(socket):
         request_literal = request.decode("ISO-8859-1")
         if request_literal[-4:] == '\r\n\r\n':
             break
-    return request
+    return request.decode()
 
 def create_response():
     response = ""
@@ -50,6 +51,14 @@ def create_response():
     response += "\r\n"
     response += "Hello!"
     return response
+
+def get_file_path(request):
+    first_line_of_request = request.split('\r\n')[0]
+    file_path = first_line_of_request.split()[1]
+    return file_path
+
+def get_file_name(path):
+    return os.path.split(path)[-1]
 
 def main():
     if not command_line_argument_exists():
@@ -64,6 +73,8 @@ def main():
         print(f"Accepting connection on {client_address}")
 
         request = get_request(connection)
+        requested_file_path = get_file_path(request)
+        requested_file_name = get_file_name(requested_file_path)
         response = create_response()
         connection.sendall(response.encode("ISO-8859-1"))
         print(f"Closing connection on {client_address}\n")
